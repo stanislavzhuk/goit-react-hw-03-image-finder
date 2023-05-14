f166e7097cd24b4ed95508ff53a6b15151534668
Stanislav-Zhuk <steys.ua@gmail.com>
2023-05-14 16:51:37 +0300

    update App and Searchbar

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,19 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import fetchImages from 'services/ApiPixabay';
+import css from './App.module.css';
+
+class App extends Component {
+  state = {
+
+  };
+
+  render() {
+    return (
+      <div className={css.App}>
+
+      </div>
+    )
+  }
+}
+
+export default App;
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,27 @@
+import { Component } from "react";
+import PropTypes from 'prop-types';
+import css from './Searchbar.module.css';
+
+class Searchbar extends Component {
+  render() {
+    return (
+      <header class="searchbar">
+        <form class="form">
+          <button type="submit" class="button">
+            <span class="button-label">Search</span>
+          </button>
+
+          <input
+            class="input"
+            type="text"
+            autocomplete="off"
+            autofocus
+            placeholder="Search images and photos"
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
