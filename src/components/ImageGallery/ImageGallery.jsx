6d1c5212c1faf93761f3f0716b19b53764e207ae
Stanislav-Zhuk <steys.ua@gmail.com>
2023-05-15 13:12:53 +0300

    update ImageGallery and ImageGalleryItem

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,3 +1,19 @@
 import { Component } from "react";
 import PropTypes from 'prop-types';
+import css from './ImageGallery.module.css';
 
+class ImageGallery extends Component {
+  render() {
+    return (
+      <ul className={css.ImageGallery}>
+
+      </ul>
+    )
+  }
+}
+
+ImageGallery.propTypes = {
+  
+};
+
+export default ImageGallery;
---
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import css from './ImageGalleryItem.module.css';
+
+const ImageGalleryItem = () => {
+  return (
+    <li class="gallery-item">
+      <img src="" alt="" />
+    </li>
+  )
+}
+
+ImageGalleryItem.propTypes = {
+  
+}
+
+export default ImageGalleryItem;
