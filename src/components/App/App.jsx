2c8dc734a85847d6856efdba53586fb44c0803e9
Stanislav-Zhuk <steys.ua@gmail.com>
2023-05-15 15:41:15 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-// import fetchImages from 'services/ApiPixabay';
+import fetchImages from 'services/ApiPixabay';
 import Searchbar from 'components/Searchbar/Searchbar';
 
 import { ToastContainer, toast } from 'react-toastify';
@@ -11,7 +11,28 @@ class App extends Component {
     searchQuery: '',
     page: 1,
     images: [],
+    loading: false,
   };
+
+  async componentDidUpdate(pervProps, prevState) {
+    const { searchQuery, page } = this.state;
+    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
+      this.setState({ loading: true });
+
+      try {
+        const { hits } = await fetchImages(searchQuery, page);
+        if (!hits.length) {
+          toast.warning('No results were found for your search, please try something else.')
+        }
+        this.setState(({ images }) => ({
+          images: [...images, ...hits],
+          loading: false,
+        }));
+      } catch (error) {
+        toast.error(`Sorry something went wrong. ${error.message}`);
+      }
+    }
+  }
 
   handleFormSubmit = searchQuery => {
     this.setState({searchQuery})
